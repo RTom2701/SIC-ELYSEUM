46e6db9591dc57dbe487c60333e46fd35fd09e1e
tom <roty@et.esiea.fr>
2023-01-30 12:26:17 +0100

    a

--- main.c
+++ main.c
@@ -19,6 +19,5 @@ int main() {
  int *tab[100000];
  for (int i=0; i<10000;i++) tab[i]=calloc(n, sizeof(int));
 
- https://code-with-me.global.jetbrains.com/BFsbpflJtl1r_GksJIcxFg#p=CL&fp=C4AC16BDC21079F997B675F3FA6B39EF96E019934F030E76FF995205300616D7
-
+https://code-with-me.global.jetbrains.com/TjIGZ05kETOxMHKnOUADqg#p=CL&fp=C4AC16BDC21079F997B675F3FA6B39EF96E019934F030E76FF995205300616D7
  */
