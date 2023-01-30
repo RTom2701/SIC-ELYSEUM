da87c9061f4d5e51d1f2e556c0e3a58c7f66bc3e
tom <roty@et.esiea.fr>
2023-01-30 11:15:53 +0100

    a

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    FILE *f;
+    char c;
+    char *s[10];
+    s[0]="ab";
+    int i=0;
+    f=fopen("10.input","rt");
+    while((c=fgetc(f))!=EOF){
+        //s[i]="a";
+        i++;
+    }
+    printf("%d", s[0]);
+    fclose(f);
+    return 0;
+}
